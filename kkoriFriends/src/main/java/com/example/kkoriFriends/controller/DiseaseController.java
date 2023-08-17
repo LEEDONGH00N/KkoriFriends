8f27575a92e32f02481b54fda231ff15c3592287
LeeDongHoon <likelion.leedh@gmail.com>
2023-08-18 05:03:43 +0900

    test-2

--- kkoriFriends/src/main/java/com/example/kkoriFriends/controller/DiseaseController.java
+++ kkoriFriends/src/main/java/com/example/kkoriFriends/controller/DiseaseController.java
@@ -8,8 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/KkoriFriends/disease")
 public class DiseaseController {
     @GetMapping("")
-    public String showDisease()
-    {
+    public String showDisease() {
         return "개-증상별 질병찾기";
+    }
+    @GetMapping("/dog")
+    public String showDogDisease() {
+        return "개-증상별 질병찾기";
+    }
+    @GetMapping("/cat")
+    public String showCatDisease() {
+        return "고양이-증상별 질병찾기";
+    }
+    @GetMapping("/fish")
+    public String showFishDisease() {
+        return "물고기-증상별 질병찾기";
+    }
+    @GetMapping("/bird")
+    public String showBirdDisease() {
+        return "앵무새-증상별 질병찾기";
     }
 }
