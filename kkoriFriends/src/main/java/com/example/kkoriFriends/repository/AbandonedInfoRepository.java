b98ace8b89a1996c251726e9e869811ff21f5415
LeeDongHoon <likelion.leedh@gmail.com>
2023-08-18 22:02:41 +0900

    최종

--- kkoriFriends/src/main/java/com/example/kkoriFriends/repository/AbandonedInfoRepository.java
+++ kkoriFriends/src/main/java/com/example/kkoriFriends/repository/AbandonedInfoRepository.java
@@ -1,7 +1,10 @@
 package com.example.kkoriFriends.repository;
 
 import com.example.kkoriFriends.entity.Abandoned;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AbandonedInfoRepository extends JpaRepository<Abandoned, Long> {
+    Page<Abandoned> findAll(Pageable pageable);
 }
